48c0f30600bf5ef03f43cb66634104d32a997c51
Nicholas Vahalik <nick@classyllama.com>
2011-03-06 22:09:10 -0600

    Added license and copyright to module plugin.

--- app/plugins/Module.php
+++ app/plugins/Module.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Wiz
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ * 
+ * This program is provided to you AS-IS.  There is no warranty.  It has not been
+ * certified for any particular purpose.
+ *
+ * @package    Wiz
+ * @author     Nick Vahalik <nick@classyllama.com>
+ * @copyright  Copyright (c) 2011 Classy Llama Studios
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
 class Wiz_Plugin_Module extends Wiz_Plugin_Abstract {
 
